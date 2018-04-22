c5b724e96e0d99ca371b5bd4b521017eebf5d225
Aida7 <33822250+Aida7@users.noreply.github.com>
2018-04-22 20:33:02 +0600

    Ef

---
+++ EfСompany/Context.cs
@@ -0,0 +1,20 @@
+﻿namespace EfСompany
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Context : DbContext
+    {
+        
+        public Context()
+            : base("name=Context")
+        {
+        }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Collaborator> Collaborators { get; set; }
+    }
+
+    
+}
